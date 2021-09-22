d10f3809048cf0fba1bc9721d10553019031c02e
Luciano <lucco6823@gmail.com>
2021-09-22 17:07:11 +0200

    Scheletro Pagina

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <img alt="Spotify logo" src="../assets/img/logo-spotify.png">
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
